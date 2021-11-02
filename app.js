fe421f7ffdfbf5c5aaf5768fa50c7b8d9e32dce5
ivanchango <ianivan2003@hotmail.com>
2021-11-01 23:18:19 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+//localhost:
+
+app.listen(3030, () => console.log ('Esto fue exitoso'));
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+    let htmlPath = path.resolve(__dirname, './views/home.html')
+    res.sendFile(htmlPath) 
+}); 
